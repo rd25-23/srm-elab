52f61cd11fa1d1afb837c49f53aebd1cac494302
Ritish Doad <rd0519@srmist.edu.in>
2026-08-08 00:02:55 +0530

    Add input handling and price calculation
    url-https://dld.srmist.edu.in/ktretelab2025/#/ktretelab2025/student/home

--- ques202.py
+++ ques202.py
@@ -1,7 +1,11 @@
 #this is question 202 from srm elab
 
+
+
 billid = int(input())
 productid = int(input())
 price = float(input())
 quantity = int(input())
 print(f"{price*quantity:.2f}")
+
+#url - https://dld.srmist.edu.in/ktretelab2025/#/ktretelab2025/student/home
