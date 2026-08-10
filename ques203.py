763da59115a065acc720ef3839bc4880c87b5da5
Ritish Doad <rd0519@srmist.edu.in>
2026-08-10 22:29:35 +0530

    Implement distance conversion in ques203.py
    Add conversion script for distance in various units
    url-https://dld.srmist.edu.in/ktretelab2025/#/ktretelab2025/student/home

---
+++ ques203.py
@@ -0,0 +1,5 @@
+#this question number 203 from srm elab
+dis = float(input())
+print(f"{dis*1000:.2f} m\n{dis*3280.84:.2f} ft\n{dis*39370.1:.2f} in\n{dis*100000:.2f} cm")
+
+# url -https://dld.srmist.edu.in/ktretelab2025/#/ktretelab2025/student/home
